e543da7b3aa64315c8604c8d2a824c6b3ea7869f
András Guseo <andras@divi-magazine.com>
2020-11-02 18:56:57 +0100

    Moved end bail ...
    ... to prevent further calculation and avoid the progress bar becoming wider than 100%

--- src/event-progress-bar.js
+++ src/event-progress-bar.js
@@ -34,13 +34,21 @@ function doProgress() {
 		var timeLeft = endDate - (now + offset);
 		var percent = (fullTime - timeLeft) / fullTime * 100;
 
+		// If the count down is over, write some text and get out of the loop.
+		if ( timeLeft < 0 ) {
+			events[ i ].children[ 0 ].innerHTML = "Event";
+			events[ i ].children[ 2 ].children[ 0 ].innerHTML = "is over.";
+			events[ i ].classList.remove( "progress-bar-container__on" );
+			continue;
+		}
+
 		// Time calculations for days, hours, minutes and seconds
 		var days = Math.floor( timeLeft / (1000 * 60 * 60 * 24) );
 		var hours = Math.floor( (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) );
 		var minutes = Math.floor( (timeLeft % (1000 * 60 * 60)) / (1000 * 60) );
 		var seconds = Math.floor( (timeLeft % (1000 * 60)) / 1000 );
 
-		// Output the result in an element with id="demo"
+		// Output the result in an element
 		var timeString = '';
 		if ( days > 0 ) timeString += days + 'd ';
 		if ( hours < 10 ) hours = '0' + hours;
@@ -51,11 +59,5 @@ function doProgress() {
 		events[ i ].children[ 2 ].children[ 0 ].innerHTML = timeString;
 		events[ i ].children[ 1 ].children[ 0 ].style.width = 'calc(' + percent + '% - 9px)';
 
-		// If the count down is over, write some text
-		if ( timeLeft < 0 ) {
-			events[ i ].children[ 0 ].innerHTML = "Event";
-			events[ i ].children[ 2 ].children[ 0 ].innerHTML = "is over.";
-			events[ i ].classList.remove( "progress-bar-container__on" );
-		}
 	}
 }
