95a787155e4f13576a49295aaab686184a186055
András Guseo <andras@divi-magazine.com>
2020-11-06 00:01:52 +0100

    Formatting

--- src/event-progress-bar.js
+++ src/event-progress-bar.js
@@ -26,7 +26,7 @@ function doProgress() {
 	var offset = tzOffset * 60 * 1000;
 
 	// Go through all the events that are running at the moment.
-	Array.prototype.forEach.call(events, function( event ) {
+	Array.prototype.forEach.call( events, function( event ) {
 		var startDate = new Date( event.querySelector( '.progress-bar-container__live' ).dataset.startDate ).getTime();
 		var endDate = new Date( event.querySelector( '.progress-bar-container__live' ).dataset.endDate ).getTime();
 
@@ -36,11 +36,11 @@ function doProgress() {
 
 		if ( timeLeft < 0 ) {
 			// If the countdown is over change the labels.
-			event.querySelector( '.progress-bar-container__live-text' ).classList.add('tribe-common-a11y-hidden');
-			event.querySelector( '.progress-bar-container__live-text--over' ).classList.remove('tribe-common-a11y-hidden');
-			event.querySelector( '.progress-bar-container__timeleft-time' ).classList.add('tribe-common-a11y-hidden');
-			event.querySelector( '.progress-bar-container__timeleft-string' ).classList.add('tribe-common-a11y-hidden');
-			event.querySelector( '.progress-bar-container__timeleft--over' ).classList.remove('tribe-common-a11y-hidden');
+			event.querySelector( '.progress-bar-container__live-text' ).classList.add( 'tribe-common-a11y-hidden' );
+			event.querySelector( '.progress-bar-container__live-text--over' ).classList.remove( 'tribe-common-a11y-hidden' );
+			event.querySelector( '.progress-bar-container__timeleft-time' ).classList.add( 'tribe-common-a11y-hidden' );
+			event.querySelector( '.progress-bar-container__timeleft-string' ).classList.add( 'tribe-common-a11y-hidden' );
+			event.querySelector( '.progress-bar-container__timeleft--over' ).classList.remove( 'tribe-common-a11y-hidden' );
 			event.classList.remove( "progress-bar-container__on" );
 		} else {
 			// Time calculations for days, hours, minutes and seconds
@@ -50,24 +50,24 @@ function doProgress() {
 			var day = hour * 24;      // (1000 * 60 * 60 * 24)
 
 			var days = Math.floor( timeLeft / day );
-			var hours = Math.floor( (timeLeft % day) / hour ).toString(10);
-			var minutes = Math.floor( (timeLeft % hour) / minute ).toString(10);
-			var seconds = Math.floor( (timeLeft % minute) / second ).toString(10);
+			var hours = Math.floor( (timeLeft % day) / hour ).toString( 10 );
+			var minutes = Math.floor( (timeLeft % hour) / minute ).toString( 10 );
+			var seconds = Math.floor( (timeLeft % minute) / second ).toString( 10 );
 
 			// Output the result in an element
 			var timeString = '';
-			
+
 			if ( days > 0 ) {
 				event.querySelector( '.progress-bar-container__timeleft-day' ).innerHTML = days;
 			} else {
-				event.querySelector( '.progress-bar-container__timeleft-day' ).classList.add('tribe-common-a11y-hidden');
-				event.querySelector( '.progress-bar-container__timeleft-day-label' ).classList.add('tribe-common-a11y-hidden');
+				event.querySelector( '.progress-bar-container__timeleft-day' ).classList.add( 'tribe-common-a11y-hidden' );
+				event.querySelector( '.progress-bar-container__timeleft-day-label' ).classList.add( 'tribe-common-a11y-hidden' );
 			}
 
 			timeString += hours.padStart( 2, 0 ) + ":" + minutes.padStart( 2, 0 ) + ":" + seconds.padStart( 2, 0 );
 
 			event.querySelector( '.progress-bar-container__timeleft .progress-bar-container__timeleft-time' ).innerHTML = timeString;
-			event.querySelector( '.progress-bar-container__background .progress-bar-container__progressbar').style.width = 'calc(' + percent + '% - 9px)';
+			event.querySelector( '.progress-bar-container__background .progress-bar-container__progressbar' ).style.width = 'calc(' + percent + '% - 9px)';
 		}
-	});
+	} );
 }
